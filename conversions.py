0cb308d4ba30dfd2a7e2fe57481f8ee267a727a2
Sam Guyer <sam.guyer@gmail.com>
2021-02-09 23:12:52 -0500

    Added mini assembly language

--- conversions.py
+++ conversions.py
@@ -1,14 +1,20 @@
 
-# -- Memory is a big array of bytes
+# -- Memory representation
+#    Memory is a sequence of bytes. The address of a byte is its
+#    index in that sequence. That's it!
 Size = 32
 Memory = [0 for _ in range(Size)]
 
 
+# -- Clear memory
+#    Set all the bits to 0
 def clear_memory():
     for i in range(len(Memory)):
         Memory[i] = 0
 
 
+# -- Convert a byte to a binary string
+#    For use in printing the memory state as bits
 def as_binary(byte):
     bin = ''
     for bit in range(8):
@@ -19,6 +25,10 @@ def as_binary(byte):
     return bin
 
 
+# -- Show contents of memory
+#    Display all the bits in memory. This format prints the
+#    bytes in order, with four bytes (32 bits) on each line
+#    The column at the left side shows the address.
 def show_memory():
     for i in range(0, len(Memory), 4):
         line = ''
@@ -28,6 +38,11 @@ def show_memory():
         print('{:3d}  {}'.format(i, line))
 
 
+# === Memory access ================================================
+
+# -- Load a byte from the given address
+#    Raise a 'Segmentation fault' error if the address is outside
+#    the legal range for the memory
 def load_byte(address):
     if address >= Size or address < 0:
         print("Seg fault")
@@ -36,6 +51,9 @@ def load_byte(address):
         return Memory[address]
 
 
+# -- Store a byte value to the given address
+#    Raise a 'Segmentation fault' error if the address is outside
+#    the legal range for the memory
 def store_byte(byte, address):
     if address >= Size or address < 0:
         print("Seg fault")
@@ -44,16 +62,20 @@ def store_byte(byte, address):
         Memory[address] = byte
 
 
+# -- Load an unsigned 8-bit int from the given address
 def load_uint8(address):
     val = load_byte(address)
     return val
 
 
+# -- Store an unsigned 8-bit int to the given address
 def store_uint8(sint, address):
     val = sint % 256
     store_byte(val, address)
 
 
+# -- Load a signed 8-bit int from the given address
+#    Decode from two's-complement representation
 def load_sint8(address):
     uint = load_uint8(address)
     if uint > 127:
@@ -63,6 +85,8 @@ def load_sint8(address):
     return sint
 
 
+# -- Store a signed 8-bit int to the given address
+#    Convert to two's complement representation
 def store_sint8(val, address):
     if val < 0:
         uint = val + 256
@@ -70,7 +94,7 @@ def store_sint8(val, address):
         uint = val
     store_uint8(uint, address)
 
-
+# -- Load an unsigned 16-bit int from the given address
 def load_uint16(address):
     high = load_byte(address)
     low = load_byte(address+1)
@@ -78,6 +102,7 @@ def load_uint16(address):
     return val
 
 
+# -- Store an unsigned 16-bit int to the given address
 def store_uint16(val, address):
     high = (val // 256) % 256
     low = val % 256
@@ -85,6 +110,7 @@ def store_uint16(val, address):
     store_byte(low, address+1)
 
 
+# -- Load a signed 16-bit int from the given address
 def load_sint16(address):
     uint = load_uint16(address)
     if uint > 32767:
@@ -94,6 +120,7 @@ def load_sint16(address):
     return sint
 
 
+# -- Store a signed 16-bit int to the given address
 def store_sint16(val, address):
     if val < 0:
         uint = val + 65536
@@ -102,71 +129,121 @@ def store_sint16(val, address):
     store_uint16(uint, address)
 
 
-all_variables = {}
+# === Variable access functions =====================================
 
-def var(name, address, type):
-    all_variables[name] = (address, type)
+# -- Make a new variable
+#    record the address and the type, which must be a string
+#    that is one of 'uint8', 'sint8', 'uint16', 'sint16'
+#    Return the new variable
+def var(address, type):
+    return (address, type)
 
 
-def get_var(name):
-    if name in all_variables:
-        (address, type) = all_variables[name]
-        if type == 'uint16':
+# -- Get the value of a var
+#    Load the value from the variable's address according to its type
+def get_var(v):
+    if type(v) is int:
+        return v
+    else:
+        (address, typ) = v
+        if typ == 'uint16':
             return load_uint16(address)
-        if type == 'sint16':
+        if typ == 'sint16':
             return load_sint16(address)
-        if type == 'uint8':
+        if typ == 'uint8':
             return load_uint8(address)
-        if type == 'sint8':
+        if typ == 'sint8':
             return load_sint8(address)
-    return None
-
-def set_var(name, val):
-    if name in all_variables:
-        (address, type) = all_variables[name]
-        if type == 'uint16':
-            store_uint16(val, address)
-        if type == 'sint16':
-            store_sint16(val, address)
-        if type == 'uint8':
-            store_uint8(val, address)
-        if type == 'sint8':
-            store_sint8(val, address)
-    return None
+        return None
 
 
-def copy_var(src, dest):
-    val = get_var(src)
+# --- Set the value of a var
+#     Store the value into the address according to its type
+def set_var(v, val):
+    (address, typ) = v
+    if typ == 'uint16':
+        store_uint16(val, address)
+    if typ == 'sint16':
+        store_sint16(val, address)
+    if typ == 'uint8':
+        store_uint8(val, address)
+    if typ == 'sint8':
+        store_sint8(val, address)
+
+
+# === Assembly-like functions =======================================
+
+# -- Move (copy) the value from the src variable to the dest
+#    variable, converting the representation if necessary.
+#    The src can also be a literal number.
+def mov(src, dest):
+    if type(src) is int:
+        val = src
+    else:
+        val = get_var(src)
     set_var(dest, val)
 
-
-def add(src1, src2, dest):
-    v1 = get_var(src1)
-    if type(src2) is int:
-        v2 = src2
-    else:
-        v2 = get_var(src2)
+# -- Add the value of op1 to op2 and store the result in op2
+#    Equivalent to op2 = op1 + op2
+#    op1 can be a literal
+def add(op1, op2):
+    v1 = get_var(op1)
+    v2 = get_var(op2)
     v = v1 + v2
-    set_var(dest, v)
+    set_var(op2, v)
 
 
-def less_than(src1, src2):
-    v1 = get_var(src1)
-    v2 = get_var(src2)
+# -- Subtract op2 from op1, store the result in op1
+#    Equivalent to op1 = op1 - op2
+#    op2 can be a literal
+def sub(op1, op2):
+    v1 = get_var(op1)
+    v2 = get_var(op2)
+    v = v1 - v2
+    set_var(op1, v)
+
+
+# -- Compare two variables, return True if op1 == op2
+#    Either op can be a literal
+def equal(op1, op2):
+    v1 = get_var(op1)
+    v2 = get_var(op2)
+    return v1 == v2
+
+
+# -- Compare two variables, return True if op1 < op2
+#    Either op can be a literal
+def less_than(op1, op2):
+    v1 = get_var(op1)
+    v2 = get_var(op2)
     return v1 < v2
 
 
-def less_than_or_equal(src1, src2):
-    v1 = get_var(src1)
-    v2 = get_var(src2)
+# -- Compare two variables, return True if op1 <= op2
+#    Either op can be a literal
+def less_than_or_equal(op1, op2):
+    v1 = get_var(op1)
+    v2 = get_var(op2)
     return v1 <= v2
 
 
-var('value', 8, 'sint16')
-var('prev',  10, 'sint8')
-set_var('value', 0)
-set_var('prev', 0)
-while less_than_or_equal('prev', 'value'):
-    copy_var('value', 'prev')
-    add('value', 5, 'value')
-    print('prev = {}  val = {}'.format(get_var('prev'), get_var('value')))
+# -- Ask the user to enter a value, store the result in op
+def read(msg, op):
+    v = int(input(msg))
+    set_var(op, v)
+
+
+# -- Print the value of op with a message (a string)
+def show(msg, op):
+    v = get_var(op)
+    fmt = msg + "{}"
+    print(fmt.format(v))
+
+
+# === Finally, the program ==========================================
+
+value = var(8, 'sint16')
+mov(0, value)
+while less_than(value, 10):
+    add(1, value)
+    show('Value is ', value)
